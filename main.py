5214f8c1e9d287dc3b669bef4c0fef4677794cd0
Daniel Hernandez Castillo <herna1003@csusm.edu>
2023-04-19 21:55:01 -0700

    Added functionality to romanToInt function. Works, but haven't included exceptions for 4's and 9's. will do that tommrow

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print("Hello world")
-print("bye world")
+
+class RomInt:
+    def romanToInt(self, s: str) -> int:  # given a roman numeral, convert it to an integer
+        result = 0
+        legend = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        letterList = list(s) # using list function to parse string into array of characters
+        #print(letterList)
+
+        #loop to iterate over my list
+        for letter in letterList:
+            for key, value in legend.items():
+                if letter == key:
+                    result = result + value
+
+        return result
+
+
+if __name__ == '__main__':
+    romInt = RomInt()
+
+    print(romInt.romanToInt("V"))
